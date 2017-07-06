698c28c5bfdce1f1982c33b7390ffb4d716ed5a6
DmitriyPoltavskiy <di77929@gmail.com>
2017-07-06 08:29:34 +0300

    Add angular material, modify angular-json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 // import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ShopListComponent } from './shop-list/shop-list.component';
 import { ShopItemComponent } from './shop-item/shop-item.component';
+
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 @NgModule({
 	declarations: [
@@ -21,7 +24,13 @@ import { ShopItemComponent } from './shop-item/shop-item.component';
 		// MdButtonModule, 
 		// MdCheckboxModule,
 		FormsModule,
+		BrowserAnimationsModule,
+		NoopAnimationsModule,
 		// HttpModule
+	],
+	exports: [
+		MdButtonModule, 
+		MdCheckboxModule
 	],
 	// providers: [],
 })
