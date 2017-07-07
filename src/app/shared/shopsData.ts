976a1c9c4d147d102f316ec9aa8b0e5cdca56f06
DmitriyPoltavskiy <di77929@gmail.com>
2017-07-07 23:50:27 +0300

    Add icons

--- src/app/shared/shopsData.ts
+++ src/app/shared/shopsData.ts
@@ -18,55 +18,57 @@ export const shopsData = [
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 4',
         price: '15',
 		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 5',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'https://pbs.twimg.com/profile_images/378800000494824811/3b7cf88f1da84e7e197139ca958ed8da.jpeg',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 6',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'http://www.cimbali.com/sites/default/files/styles/image_for_link_ricette/public/irish_coffee-box.jpg?itok=sl0EqUTo',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 7',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'https://pbs.twimg.com/profile_images/378800000369553334/6d5df0b0161554d354c69d8b2dc665bc.jpeg',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 8',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'http://cdn1.medicalnewstoday.com/content/images/headlines/283/283750/coffee.jpg',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 9',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'https://1.imagecdn.tockify.com/58d575fbdf82fe5738084584/58d58744df82fe5738084cfd/scaled_256.jpg',
 		isEdit: false
 	},
 	{
-		name: 'Shop 1',
+		name: 'Shop 10',
         price: '15',
-		imageUrl: 'https://is2-ssl.mzstatic.com/image/thumb/Purple111/v4/d8/78/24/d87824f8-4468-d5c1-3835-19971af4e809/source/256x256bb.jpg',
+		imageUrl: 'https://static.wixstatic.com/media/dcf6d0_ba8cbf2301b74bb5bfc454e24f654db7.jpg_256',
 		isEdit: false
 	},
-	// {
-	// 	name: 'Shop 5',
-    //     price: '5',
-		// imageUrl: 'http://cdn1.medicalnewstoday.com/content/images/headlines/311/311569/coffee.jpg'
-	// },
-	// {
-	// 	name: 'Shop 6',
-    //     price: '1',
-	// 	imageUrl: 'http://assets.centralindex.com/W/48/7271a47c04a7bd48f9a9d06bfc66966a.jpg'
-	// },
+	{
+		name: 'Shop 11',
+        price: '5',
+		imageUrl: 'http://static.wixstatic.com/media/c2c903_ea7e0e89625340e4ad3531febfd697ae.jpg_256',
+		isEdit: false
+	},
+	{
+		name: 'Shop 12',
+        price: '1',
+		imageUrl: 'http://assets.centralindex.com/W/48/7271a47c04a7bd48f9a9d06bfc66966a.jpg',
+		isEdit: false
+	},
 ];
